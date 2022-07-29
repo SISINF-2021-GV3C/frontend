899c4bdb8af9be6011ebb84086dfb1600b750e08
Paul Huszak <paulhuszak@gmail.com>
2022-07-29 19:15:00 +0200

    chart.js
    añadido el gráfico de cada moneda

---
+++ src/components/SelectButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SelectButton = ({ children, selected, onClick }) => {
+    return (
+        <span onClick={onClick} className={selected ? 'select-button-active' : 'select-button'}>
+            {children}
+        </span>
+    );
+};
+
+export default SelectButton;
--- src/pages/Asset.js
+++ src/pages/Asset.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { SingleCoin } from "../data/CoinGecko_API";
+import CoinChart from "../components/Chart"
 import Loading from "../components/Loader";
 import Collapsible from 'react-collapsible';
 import "../css/asset.css"
@@ -14,7 +15,7 @@ function Asset () {
     const [loading, setLoading] = useState(true);
     const [descText, setDescText] = useState("Leer descripción");
     const [description, setDescription] = useState("");
-
+    const [market_data, setMarket_Data] = useState([]);
     var isPositive = [];
 
     // Opciones de formato de número (USD)
@@ -24,14 +25,15 @@ function Asset () {
 
     // Descargar datos a través de la API de CoinGecko
     useEffect(() => {
-        const fetchCoins = async () => {
+        const fetchSingleCoin = async () => {
             const { data } = await axios.get(SingleCoin(id))
             setCoin(data);
+            setMarket_Data(data.market_data);
             setDescription(data.description.en);
         }; 
-        fetchCoins();
-        setTimeout(() => setLoading(false), 750);
-    }, [id]); 
+        fetchSingleCoin();
+        setTimeout(() => setLoading(false), 1500);
+    }, [id]);         
 
     // Abrir la descripción
     const openDescription = () => {
@@ -51,30 +53,101 @@ function Asset () {
     const displayTitle = () => {
         return (
             <div className="crypto-title">
-                <img className="crypto-image" src={coin.image.large} alt={coin.name} width="60px" />  
+                <img className="crypto-image" src={coin.image.large} alt={coin.name} width="50px" />  
                 | {coin.name} | 
                 <span className="crypto-symbol">{(coin.symbol).toUpperCase()}</span>
             </div>
         )
     }
 
+    // Funciones para comprobar el porcentaje de cambio para aplicar CSS
+    // Comprobar porcentaje de cambio en 24h
+    const checkPte24h = () => {
+        if (market_data.price_change_percentage_24h > 0) {
+            isPositive[0] = true;
+        } else {
+            isPositive[0] = false;
+        }
+    }
+
+    // Comprobar porcentaje de cambio en 7 días
+    const checkPte7d = () => {
+        if (market_data.price_change_percentage_7d > 0) {
+            isPositive[1] = true;
+        } else {
+            isPositive[1] = false;
+        }
+    }
+
+    // Comprobar porcentaje de cambio en 14 días
+    const checkPte14d = () => {
+        if (market_data.price_change_percentage_14d > 0) {
+            isPositive[2] = true;
+        } else {
+            isPositive[2] = false;
+        }
+    }
+
+    // Comprobar porcentaje de cambio en 30 días
+    const checkPte30d = () => {
+        if (market_data.price_change_percentage_30d > 0) {
+            isPositive[3] = true;
+        } else {
+            isPositive[3] = false;
+        }
+    }
+
+    // Comprobar porcentaje de cambio en 60 días
+    const checkPte60d = () => {
+        if (market_data.price_change_percentage_60d > 0) {
+            isPositive[4] = true;
+        } else {
+            isPositive[4] = false;
+        }
+    }
+
+    // Comprobar porcentaje de cambio en 200 días
+    const checkPte200d = () => {
+        if (market_data.price_change_percentage_200d > 0) {
+            isPositive[5] = true;
+        } else {
+            isPositive[5] = false;
+        }
+    }
+
+    // Comprobar porcentaje de cambio en 1 año
+    const checkPte1y = () => {
+        if (market_data.price_change_percentage_1y > 0) {
+            isPositive[6] = true;
+        } else {
+            isPositive[6] = false;
+        }
+    }
+
     // Mostrar la descripción de la moneda
     const displayDescription = () => {
         return (
             <div className="row">
-            <div className="read-more-container">
-                <Collapsible onTriggerOpening={openDescription} onTriggerClosing={closeDescription} trigger={descText}>
-                    <p className="description-container">            
-                        <div dangerouslySetInnerHTML={{ __html:description}}></div>
-                    </p>
-                </Collapsible>
-            </div>
+                <div className="read-more-container">
+                    <Collapsible onTriggerOpening={openDescription} onTriggerClosing={closeDescription} trigger={descText}>
+                        <div className="description-container">            
+                            <div dangerouslySetInnerHTML={{ __html:description}}></div>
+                        </div>
+                    </Collapsible>
+                </div>
             </div>
         )
     }
 
     // Mostrar valores de mercado de la moneda
     const displayMarket = () => {
+        checkPte24h();
+        checkPte7d();
+        checkPte14d();
+        checkPte30d();
+        checkPte60d();
+        checkPte200d();
+        checkPte1y();
         return (
             <div>
                 <div className="row align-items-start">
@@ -112,6 +185,22 @@ function Asset () {
                 <div className="row align-items-start">
                     <div className="col-sm mktdata-container">
                         <label className="label">
+                            Suministro total: 
+                        </label>
+                        <br></br>
+                        <span className="mktdata-prop">{supplyFormat.format(coin.market_data.total_supply)}</span>
+                    </div>
+                    <div className="col-sm mktdata-container">
+                        <label className="label">
+                            Suministro en circulación: 
+                        </label>
+                        <br></br>
+                        <span className="mktdata-prop">{supplyFormat.format(coin.market_data.circulating_supply)}</span>
+                    </div>
+                </div>
+                <div className="row align-items-start">
+                    <div className="col-sm mktdata-container">
+                        <label className="label">
                             Máximo 24h: 
                         </label>
                         <br></br>
@@ -138,7 +227,7 @@ function Asset () {
                             Pte. cambio (24h):  
                         </label>
                         <br></br>
-                        <span className="mktdata-prop">{coin.market_data.price_change_percentage_24h} % </span>
+                        <span className={`${isPositive[0] ? 'pte-success' : 'pte-danger'}`}>{coin.market_data.price_change_percentage_24h} % </span>
                     </div>
                 </div>
                 <div className="row align-items-start">
@@ -147,21 +236,21 @@ function Asset () {
                             Pte. cambio (7d): 
                         </label>
                         <br></br>
-                        <span className="mktdata-prop">{coin.market_data.price_change_percentage_7d} %</span>
+                        <span className={`${isPositive[1] ? 'pte-success' : 'pte-danger'}`}>{coin.market_data.price_change_percentage_7d} %</span>
                     </div>
                     <div className="col-sm mktdata-container-pte">
                         <label className="label">
                             Pte. cambio (14d): 
                         </label>
                         <br></br>
-                        <span className="mktdata-prop">{coin.market_data.price_change_percentage_14d} %</span>
+                        <span className={`${isPositive[2] ? 'pte-success' : 'pte-danger'}`}>{coin.market_data.price_change_percentage_14d} %</span>
                     </div>
                     <div className="col-sm mktdata-container-pte">
                         <label className="label">
                             Pte. cambio (30d): 
                         </label>
                         <br></br>
-                        <span className="mktdata-prop">{coin.market_data.price_change_percentage_30d} %</span>
+                        <span className={`${isPositive[3] ? 'pte-success' : 'pte-danger'}`}>{coin.market_data.price_change_percentage_30d} %</span>
                     </div>
                 </div>
                 <div className="row align-items-start">                   
@@ -170,21 +259,21 @@ function Asset () {
                             Pte. cambio (60d): 
                         </label>
                         <br></br>
-                        <span className="mktdata-prop">{coin.market_data.price_change_percentage_60d} %</span>
+                        <span className={`${isPositive[4] ? 'pte-success' : 'pte-danger'}`}>{coin.market_data.price_change_percentage_60d} %</span>
                     </div>
                     <div className="col-sm mktdata-container-pte">
                         <label className="label">
                             Pte. cambio (200d): 
                         </label>
                         <br></br>
-                        <span className="mktdata-prop">{coin.market_data.price_change_percentage_200d} %</span>
+                        <span className={`${isPositive[5] ? 'pte-success' : 'pte-danger'}`}>{coin.market_data.price_change_percentage_200d} %</span>
                     </div>
                     <div className="col-sm mktdata-container-pte">
                         <label className="label">
                             Pte. cambio (1y): 
                         </label>
                         <br></br>
-                        <span className="mktdata-prop">{coin.market_data.price_change_percentage_1y} %</span>
+                        <span className={`${isPositive[6] ? 'pte-success' : 'pte-danger'}`}>{coin.market_data.price_change_percentage_1y} %</span>
                     </div>
                 </div>
             </div>
@@ -194,12 +283,19 @@ function Asset () {
     return (
         <>
         {loading === false ? (
-            <div className="asset-container">
-                <div className="sidebar">
-                    <div className="title">
-                        {displayTitle()}
-                        {displayDescription()}
-                        {displayMarket()}
+            <div className="div-asset-container">
+                <div className="asset-container">
+                    <div className="sidebar_l">
+                        <div className="title">
+                            {displayTitle()}
+                            {displayDescription()}
+                            {displayMarket()}
+                        </div>
+                    </div>
+                </div>
+                <div className="chart-container">
+                    <div className="sidebar_r">
+                        <CoinChart coin={(coin)} />
                     </div>
                 </div>
             </div>
