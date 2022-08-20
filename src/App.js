16541593ef6d2ac8c2b7bb8bb0766a29edc80ef1
Paul Huszak <paulhuszak@gmail.com>
2022-08-20 20:22:34 +0200

    no coinlist

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import CoinList from "./pages/CoinList";
+//import CoinList from "./pages/CoinList";
 //import Asset from "./pages/Asset";
 import Profile from "./pages/Profile";
 import NotFound from "./components/NotFound";
@@ -27,7 +27,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/coins" element={<CoinList />} />
         <Route path="/loading" element={<Loading />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="*" element={<NotFound />} />
