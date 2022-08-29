de5ea8c54886a4e75c21844b3b48d1663493154f
a780131 <devid.dokash@gmail.com>
2022-08-29 23:17:28 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,13 +6,13 @@ NC='\033[0m'
 
 if [ $# -eq 0 ]; then echo -e "\nusage: ./deploy.sh <your_docker_username>\n"; exit 1; fi
 
-echo -e "BUILDING IMAGE ${ORN}$1/sinforeact-app${NC}. . ."
-docker build -t $1/sinforeact-app .
-if [ $? -ne 0 ]; then 
-    echo -e "\n> The build has given an ${RED}error, ${NC}did you check if Docker is available or is executed?"
-    echo -e "> If it is, ${RED}maybe is our fault${NC}, but we've checked that this works as it should.\n"
-    exit 1
-fi
+echo -e "BUILDING IMAGE ${ORN}$1/cryptohalic-app${NC}. . ."
+docker build -t $1/cryptohalic-app .
 
-echo -e "\nYour application is available in ${GRN}http://localhost:8080/\n ${NC}"
-docker run -p 8080:80 $1/sinforeact-app
+if [ $? -ne 0 ]; then exit 0; fi
+
+echo -e "\nYour application is available in ${GRN}http://localhost:8080/\n${NC}"
+echo -e "Do not close your terminal! If you want to quit first end the container process with ^C"
+
+docker run -p 8080:80 --name cryptoaholic-container $1/cryptohalic-app
+docker rm cryptoaholic-containerm
